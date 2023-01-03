5701ff94851d2e22eeef0785c7585944ffd1ff2b
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-01-02 22:55:39 -0300

    Bug fixed

--- scenes/Bat_enemy.gd
+++ scenes/Bat_enemy.gd
@@ -11,29 +11,30 @@ var directionResult = Vector2.ZERO;
 var count :float = 0.0;
 
 
+
 func _physics_process(delta :float) -> void:
 	
 	count += self.ACCELERATION * delta;
-	print("count: ",str(count));
+	#print("count: ",str(count));
+	
 	
 	if(count > 15.0):
-		var directionResult = generatePositionBat();
+		directionResult = generatePositionBat();
 		count = 0.0;
 	
-	#directionResult.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left");
-	#directionResult.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up");
+	
 	directionResult = directionResult.normalized();
 			
-			
+	animationTree.set("parameters/Parado/blend_position", directionResult);
+	animationTree.set("parameters/Voando/blend_position", directionResult);
+		
+		
 	if (directionResult != Vector2.ZERO):
-		animationTree.set("parameters/Parado/blend_position", directionResult);
-		animationTree.set("parameters/Voando/blend_position", directionResult);
 		playerSpeed = playerSpeed.move_toward(directionResult, self.ACCELERATION * delta);
-		if (animationState.is_playing()):
-			animationState.travel("Voando");
+		animationState.travel("Voando");
 	else:
-		if (animationState.is_playing()):
-			animationState.travel("Parado");
+		#if (animationState.is_playing()):
+		animationState.travel("Parado");
 		playerSpeed = playerSpeed.move_toward(Vector2.ZERO, self.FRICTION * delta);
 	
 	
@@ -41,11 +42,12 @@ func _physics_process(delta :float) -> void:
 	move_and_collide(playerSpeed);  # aplicando os valores de 'playerSpeed' para a funcao acumulativa
 
 
+
 func generatePositionBat() -> Vector2:
 	var directionRandom = Vector2.ZERO;
 	directionRandom.x = -1 if (int(rand_range(0, 2))==0) else 1 ;
 	directionRandom.y =  -1 if (int(rand_range(0, 2))==0) else 1 ;
-	#print("X: ",str(directionRandom.x), " Y: ",str(directionRandom.y));
+	# print("X: ",str(directionRandom.x), " Y: ",str(directionRandom.y));
 	
 	return directionRandom;
 	
