ebb06b1b5052a71fa2d526507b8d41784c2717da
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-01-24 23:44:50 -0300

    Trying make tower rotation

--- scenes/water_tower_rotate.gd
+++ scenes/water_tower_rotate.gd
@@ -1,4 +1,16 @@
 extends Node2D
 
+var count :float = 0.0;
+var modifyIncrement :int= 1;
+
 func _process(delta):
-	print("rodando torre...");
+	var tower = self.get_node("OceanTower");
+	count += delta #* modifyIncrement ;
+	
+	if((count > 4.5) or (count < -4.5)):
+		#modifyIncrement *= -1;
+		print("delta: ",delta);
+	
+	tower.rotate( deg2rad(count) );
+	
+	print("count: ",count);
