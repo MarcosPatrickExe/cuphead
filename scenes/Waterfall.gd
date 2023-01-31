af83fd9757d4f2441d23a55580f2f64daa93f71c
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-01-30 23:58:27 -0300

    Waterfall animation donegit add .

--- scenes/Waterfall.gd
+++ scenes/Waterfall.gd
@@ -7,15 +7,16 @@ func _process(delta):
 	
 	count +=delta;
 	
-	if(count > 1):
+	if(count > 0.5):
 		for n in range(11, 71):
 			var nodeName = "PokemonExteriorTileset"+str(n);
+			print("nam: --",nodeName);
 			var tile = self.get_node(nodeName);
-			tile.set_position( Vector2(tile.position.x, tile.position.y+8.75) );
+			tile.set_position( Vector2(tile.position.x, tile.position.y+8) );
 			
 			
-			if(tile.position.y > 95):
-				tile.set_position( Vector2(tile.position.x, 8) );
+			if(tile.position.y >= 110):
+				tile.set_position( Vector2(tile.position.x, 14.75) );
 			
 			# print("position: ",tile.position.y);
 			# tile1.position(tile1.get_position_in_parent());
