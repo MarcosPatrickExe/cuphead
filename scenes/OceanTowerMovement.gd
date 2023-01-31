14fb2cbf11d539884fe8af0e2a05b1405b919393
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-01-31 13:16:43 -0300

    Addying more colored trees

--- scenes/OceanTowerMovement.gd
+++ scenes/OceanTowerMovement.gd
@@ -7,10 +7,10 @@ func _process(delta):
 	var tower = self.get_node("ocean_tower_sprite");
 	count += (delta * modifyIncrement) ;
 	
-	if((count >= 0.5)):
+	if((count >= 0.450789)):
 		modifyIncrement *= -1;
-	if((count <= -0.5)):
+	if((count <= -0.450789)):
 		modifyIncrement *= -1;
 		
-	#print("count: ",count);
+	print("count: ",count);
 	tower.rotate( deg2rad(count) );
--- scenes/Waterfall.gd
+++ scenes/Waterfall.gd
@@ -1,24 +1,22 @@
 extends Node2D;
 
-var count: float =0;
+var water_speed: float;
 
 
 func _process(delta):
 	
-	count +=delta;
+	water_speed +=delta;
 	
-	if(count > 0.5):
+	if(water_speed > 0.3):
 		for n in range(11, 71):
 			var nodeName = "PokemonExteriorTileset"+str(n);
-			print("nam: --",nodeName);
 			var tile = self.get_node(nodeName);
 			tile.set_position( Vector2(tile.position.x, tile.position.y+8) );
-			
 			
 			if(tile.position.y >= 110):
 				tile.set_position( Vector2(tile.position.x, 14.75) );
 			
 			# print("position: ",tile.position.y);
 			# tile1.position(tile1.get_position_in_parent());
-			count = 0;
+			water_speed = 0;
 	
