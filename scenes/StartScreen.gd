ab2184f098574b3dd0aae43ac47321eb6b52f123
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-02-16 16:11:06 -0300

    Script done for initial scene

--- scenes/StartScreen.gd
+++ scenes/StartScreen.gd
@@ -1,13 +1,15 @@
 extends Control
 onready var timer = $Timer;
+onready var text = $text;
 # var active :bool = false;
 
+"""
 func _process(delta :float) -> void:
 	
-	if( self.timer.wait_time != 0 ):
-		self.get_node("text").visible = true;
-		
+	# if( self.timer.wait_time != 0 ):
+	#	self.get_node("text").visible = true;
+"""
 
 func _on_Timer_timeout():
-	print("Atual wait time: ",self.timer.wait_time );
-	get_node("text").visible = false;
+	
+	self.get_node("text").visible = not get_node("text").visible
