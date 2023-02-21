2a22e46485043e46e32a5e7c0af53cc59956ea5a
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-02-20 23:57:44 -0300

    Addying a gradient resource for game

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -1,10 +1,16 @@
 extends Control;
 onready var stopButton = $stopButton;
-
+onready var backButton = $backButton;
 
 func _ready() -> void:
-	self.stopButton.connect("pressed", self, "button_pressed");
+	self.stopButton.connect("pressed", self, "gameOffButtonPressed");
+	self.backButton.connect("pressed", self, "backButtonPressed")
 	
 	
-func button_pressed():
+func gameOffButtonPressed():
 	self.get_tree().quit();
+
+
+func backButtonPressed():
+	self.get_tree().change_scene("res://scenes/StartScreen.tscn");
+
