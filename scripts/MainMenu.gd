57a05eac241a4a9440401429d38dfc4e7d9e1963
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-02-23 19:58:44 -0300

    Addying more UI elements for main menu

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -2,14 +2,16 @@ extends Control;
 onready var stopButton = $stopButton;
 onready var backButton = $backButton;
 
+
 func _ready() -> void:
 	self.stopButton.connect("pressed", self, "gameOffButtonPressed");
 	self.backButton.connect("pressed", self, "backButtonPressed")
 	
+
+	
 	
 func gameOffButtonPressed():
 	self.get_tree().quit();
-
 
 func backButtonPressed():
 	self.get_tree().change_scene("res://scenes/StartScreen.tscn");
