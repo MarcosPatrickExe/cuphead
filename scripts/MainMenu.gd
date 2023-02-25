ff69f78db95f1825b0a8c7f5f18655d002168b02
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-02-25 01:02:55 -0300

    Testing rotation for menu options

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -1,6 +1,13 @@
 extends Control;
-onready var stopButton = $quitButton;
+onready var quitButton = $quitButton;
 onready var backButton = $backButton;
+onready var arenaOption = $arenaOption;
+onready var multiplaer = $multiplayerOption;
+onready var pvp = $pvpOption;
+onready var settings = $settingsOption;
+onready var single = $singleOption;
+var angle :float = 0;
+const speed :float = 190.0;
 
 
 func _ready() -> void:
@@ -14,3 +21,19 @@ func gameOffButtonPressed():
 func backButtonPressed():
 	self.get_tree().change_scene("res://scenes/StartScreen.tscn");
 
+
+
+func _process(delta:float) ->void:
+	angle += (delta * speed);
+	print(angle);
+	
+	var pos = Vector2( 
+			cos( deg2rad(angle)) *40.0, 
+			sin( deg2rad(angle)) *40.0
+		) + Vector2(160 ,75);
+	
+	self.arenaOption.set_position( pos );
+	
+	
+	
+	
--- scripts/ParallaxStartScreenBackgroundScript.gd
+++ scripts/ParallaxStartScreenBackgroundScript.gd
@@ -5,3 +5,4 @@ export var background_speedXY = -15;
 func _process(delta :float) -> void:
 	self.motion_offset.x += (self.background_speedXY * delta);
 	self.motion_offset.y -= (self.background_speedXY * delta);
+	# print("offsetX: ",str(self.motion_offset.x));
