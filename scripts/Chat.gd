89d280999ab9a3facabdfc24582d1511c5e307a5
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-03-06 13:24:50 -0300

    Addying a modal popup to create user on chat

--- scripts/Chat.gd
+++ scripts/Chat.gd
@@ -3,10 +3,17 @@ onready var backBtn = $backButton;
 onready var textInput = $comment_panel/TextEdit;
 onready var commentScene = preload("res://scenes/CommentTemplate.tscn");
 onready var commentVContainer = $chat_panel/ScrollContainer/VBoxContainer;
+const userName :String = "";
 
 
 func _ready():
 	self.backBtn.connect("pressed", self, "backBtnPressed");
+	
+
+func _process(delta :float) -> void:
+	if(Input.is_action_pressed("space") ):
+		$create_user_window.popup_centered( );
+
 
 
 func backBtnPressed():
