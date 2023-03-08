eb8aa9b15b31a4147175d47774eb756bef48381d
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-03-08 18:05:55 -0300

    Almost implement the fade-in effect

--- scripts/Chat.gd
+++ scripts/Chat.gd
@@ -5,7 +5,8 @@ onready var commentScene = preload("res://scenes/CommentTemplate.tscn");
 onready var commentVContainer = $chat_panel/ScrollContainer/VBoxContainer;
 onready var shadowForWindowCreateUser = $shadow_for_dialog;
 const userName :String = "";
-var able :bool = false;
+var ableToShow :bool = false;
+var abletoHide :bool = false;
 var styleboxPanel;
 
 
@@ -14,22 +15,36 @@ func _ready():
 	self.backBtn.connect("pressed", self, "backBtnPressed");
 	self.styleboxPanel = self.shadowForWindowCreateUser.get_stylebox("panel", "");
 	self.styleboxPanel.bg_color.a = 0 ;
-
 	
+#	print("ready: shadow is visible: ",self.shadowForWindowCreateUser.visible);
+
+
 
 func _process(delta :float) -> void:
 	
-	if(Input.is_action_pressed("space") ):
-		able = true;
+	if(Input.is_action_pressed("space") && !$create_user_window.visible ):
+		ableToShow = true;
 		$create_user_window.popup_centered();
-		
 
-	if(able):
+
+	if(ableToShow):
 		self.styleboxPanel.bg_color.a += delta;
 		
-		if(self.styleboxPanel.bg_color.a >= 0.7):
-			self.able = false;
+		if(self.styleboxPanel.bg_color.a >= 0.774):
+			self.ableToShow = false;
+			
+	elif(abletoHide):
+		self.styleboxPanel.bg_color.a -= delta;
+		
+		if(self.styleboxPanel.bg_color.a <= 0.0):
+			self.abletoHide = false;
 
+	if( !$create_user_window.visible ):
+		self.abletoHide = true;
+	
+
+
+	print("is visible: ", $create_user_window.visible)
 
 
 func backBtnPressed():
@@ -49,3 +64,6 @@ func _on_sendBtn_pressed():
 	
 	self.textInput.text = "";
 	self.commentVContainer.add_child(commentSceneInstance);
+
+
+
