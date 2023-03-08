5afcbcc4c56a5ff46032879e64a1121ba47f7439
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-03-08 00:06:28 -0300

    Fade-in effect assign to create_user_window

--- scripts/Chat.gd
+++ scripts/Chat.gd
@@ -5,7 +5,6 @@ onready var commentScene = preload("res://scenes/CommentTemplate.tscn");
 onready var commentVContainer = $chat_panel/ScrollContainer/VBoxContainer;
 onready var shadowForWindowCreateUser = $shadow_for_dialog;
 const userName :String = "";
-var newColor : Color;
 var able :bool = false;
 var styleboxPanel;
 
@@ -13,23 +12,23 @@ var styleboxPanel;
 
 func _ready():
 	self.backBtn.connect("pressed", self, "backBtnPressed");
-	self.styleboxPanel = self.shadowForWindowCreateUser.get_stylebox("panel", "");							
+	self.styleboxPanel = self.shadowForWindowCreateUser.get_stylebox("panel", "");
+	self.styleboxPanel.bg_color.a = 0 ;
+
 	
 
 func _process(delta :float) -> void:
-	
 	
 	if(Input.is_action_pressed("space") ):
 		able = true;
 		$create_user_window.popup_centered();
 		
-	#	var types = self.shadowForWindowCreateUser.get get_stylebox_types();
-	
-	#	print("stylebox color: ", names.bg_color) ;
-		
+
 	if(able):
-		self.newColor.a += delta *100;
-		self.styleboxPanel.bg_color= self.newColor ;
+		self.styleboxPanel.bg_color.a += delta;
+		
+		if(self.styleboxPanel.bg_color.a >= 0.7):
+			self.able = false;
 
 
 
