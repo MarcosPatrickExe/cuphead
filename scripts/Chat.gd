6374601929b9a02f349fd1802695a9305ab254c2
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-03-07 23:59:45 -0300

    bg_color from styleboxflat

--- scripts/Chat.gd
+++ scripts/Chat.gd
@@ -3,16 +3,33 @@ onready var backBtn = $backButton;
 onready var textInput = $comment_panel/TextEdit;
 onready var commentScene = preload("res://scenes/CommentTemplate.tscn");
 onready var commentVContainer = $chat_panel/ScrollContainer/VBoxContainer;
+onready var shadowForWindowCreateUser = $shadow_for_dialog;
 const userName :String = "";
+var newColor : Color;
+var able :bool = false;
+var styleboxPanel;
+
 
 
 func _ready():
 	self.backBtn.connect("pressed", self, "backBtnPressed");
+	self.styleboxPanel = self.shadowForWindowCreateUser.get_stylebox("panel", "");							
 	
 
 func _process(delta :float) -> void:
+	
+	
 	if(Input.is_action_pressed("space") ):
+		able = true;
 		$create_user_window.popup_centered();
+		
+	#	var types = self.shadowForWindowCreateUser.get get_stylebox_types();
+	
+	#	print("stylebox color: ", names.bg_color) ;
+		
+	if(able):
+		self.newColor.a += delta *100;
+		self.styleboxPanel.bg_color= self.newColor ;
 
 
 
