c7c3095014b3dd6510e7e5f384fda3d061a55f56
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-03-16 16:12:29 -0300

    Addying a select player wireframe

---
+++ scripts/SelectPlayer.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+
+
+func _ready():
+	$backButton.connect("pressed", self, "backBtnPressed");
+
+
+
+
+func backBtnPressed():
+	get_tree().change_scene("res://scenes/MainMenu.tscn");
