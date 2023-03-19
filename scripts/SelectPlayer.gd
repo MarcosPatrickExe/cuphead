068a673c637cb9d8f4aca468596ca685d2017760
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-03-19 18:50:16 -0300

    almost completing the character sealing screen

--- scripts/SelectPlayer.gd
+++ scripts/SelectPlayer.gd
@@ -1,21 +1,29 @@
 extends Control;
+# var funcc = funcref(self, "mouseOnImage")
 
 
 func _ready():
 	$backButton.connect("pressed", self, "backBtnPressed");
-	
+	$menuSelect/foxBTN.connect("mouse_entered", self, "mouseOnButton", [$menuSelect/foxBTN]);
+	$menuSelect/foxBTN.connect("mouse_exited", self, "mouseLeaveOfButton", [$menuSelect/foxBTN]);
+
 
 func backBtnPressed():
 	get_tree().change_scene("res://scenes/MainMenu.tscn");
 
 
 
+func mouseOnButton(button :TextureButton):
+	print("mouse em cima da fox!!");
+	
+	button.get_child(0).set_scale( Vector2(1.09, 1.07) );
+	button.get_child(0).material.set_shader_param("isSelected", true);
 
-func _on_soraBTN_mouse_entered():
-	$menuSelect/soraBTN/soraBorder.set_scale( Vector2(1.09, 1.07) );
-	$menuSelect/soraBTN/soraBorder.material.set_shader_param("isSelected", true);
 
 
-func _on_soraBTN_mouse_exited():
-	$menuSelect/soraBTN/soraBorder.set_scale( Vector2(1.05, 1) );
-	$menuSelect/soraBTN/soraBorder.material.set_shader_param("isSelected", false);
+func mouseLeaveOfButton(button :TextureButton):
+	print("mouse fora");
+	button.get_child(0).set_scale( Vector2(1.05, 1) );
+	button.get_child(0).material.set_shader_param("isSelected", false);
+
+	
