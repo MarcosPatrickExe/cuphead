e008f32975427b66b42304dc130c617e101d63aa
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-03-20 11:57:43 -0300

    Addying a selection characters by keyboard

--- scripts/SelectPlayer.gd
+++ scripts/SelectPlayer.gd
@@ -1,5 +1,27 @@
 extends Control;
 # var funcc = funcref(self, "mouseOnImage")
+var currSelection = Vector2(0, 0); # current selection index array
+
+
+
+var charactersOptionsMap :Dictionary = {
+	0 : {
+		0: $menuSelect/frogBTN,
+		1: $menuSelect/foxBTN,
+		2: $menuSelect/chronoBTN 
+	},
+	1 : {
+		0: $menuSelect/magusBTN,
+		1: $menuSelect/aylaBTN,
+		2: $menuSelect/blackMageBTN
+	},
+	2 : {
+		0: $menuSelect/ganbareGoemonBTN,
+		1: $menuSelect/linkBTN,
+		2: $menuSelect/soraBTN
+	}
+}
+
 
 
 
@@ -12,19 +34,72 @@ func _ready():
 
 
 
+
+func _input(event :InputEvent):
+	if(Input.is_key_pressed(KEY_UP) || Input.is_key_pressed(KEY_W) &&  (self.currSelection.y -1 >= 0)):
+		self.currSelection.y -= 1;
+		
+		self.mouseLeaveOfButton(
+			self.charactersOptionsMap[currSelection.x][currSelection.y]
+		);
+	#	print("up from input");
+	
+	
+
+
+func mouseOnButton(button :TextureButton):
+	# print("mouse em cima do node: ",button.name);
+	button.get_child(0).set_scale( Vector2(1.09, 1.07) );
+	button.get_child(0).material.set_shader_param("isSelected", true);
+	
+	
+func mouseLeaveOfButton(button :TextureButton):
+	button.get_child(0).set_scale( Vector2(1.05, 1) );
+	button.get_child(0).material.set_shader_param("isSelected", false);
+
+
+
 func backBtnPressed():
 	get_tree().change_scene("res://scenes/MainMenu.tscn");
 
 
 
-func mouseOnButton(button :TextureButton):
-	print("mouse em cima do node: ",button.name);
-	button.get_child(0).set_scale( Vector2(1.09, 1.07) );
-	button.get_child(0).material.set_shader_param("isSelected", true);
-	
-func mouseLeaveOfButton(button :TextureButton):
-	print("mouse fora do node: ", button.name);
-	button.get_child(0).set_scale( Vector2(1.05, 1) );
-	button.get_child(0).material.set_shader_param("isSelected", false);
+var aylaCharacter :Dictionary = {
+	"current": $menuSelect/aylaBTN,
+	"top": self.foxCharacter,
+	"right": self.blackMageCharacter,
+	"bottom": self.linkCharacter,
+	"left": self.magusCharacter
+}
 
-	
+var blackMageCharacter :Dictionary = {
+	"current": $menuSelect/blackMageBTN,
+	"top": self.chronoCharacter,
+	"right": blackMageCharacter,
+	"bottom": self.soraCharacter,
+	"left": self.aylaCharacter
+}
+
+var chronoCharacter :Dictionary = {
+	"current": $menuSelect/chronoBTN,
+	"top": chronoCharacter,
+	"right": chronoCharacter,
+	"bottom": self.blackMageCharacter,
+	"left": self.foxCharacter
+}
+
+var foxCharacter :Dictionary = {
+	"current": $menuSelect/foxBTN,
+	"top": foxCharacter,
+	"right": self.chronoCharacter,
+	"bottom": self.aylaCharacter,
+	"left": self.frogCharacter
+}
+
+var frogCharacter :Dictionary = {
+	"current": $menuSelect/frogBTN,
+	"top": self.frogCharacter,
+	"right": self.foxCharacter,
+	"bottom": self.magusCharacter,
+	"left": self.frogCharacter
+}
