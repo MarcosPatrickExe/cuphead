331e980181accccabf3b01f2e7ceff83e810f183
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-03-27 22:33:13 -0300

    Addying Ayla movimentation

--- scripts/Ayla_player.gd
+++ scripts/Ayla_player.gd
@@ -1,8 +1,12 @@
-extends StaticBody2D;
+extends KinematicBody2D;
 onready var animations = $AnimationPlayer;
 enum Directions { RIGHT, DOWN, LEFT, UP };
 var currentDirection;
 var dirCod :int;
+var playerCood = Vector2(0, 0);
+var directionValues = Vector2(0, 0);
+
+
 
 
 func _ready() -> void:
@@ -14,16 +18,21 @@ func _physics_process(delta :float) -> void:
 	
 	if(Input.is_action_just_pressed("a")):
 		self.currentDirection = Directions.LEFT;
-		
+		self.directionValues.x = -1;
 	elif(Input.is_action_just_pressed("w")):
 		self.currentDirection = Directions.UP;
-		
+		self.directionValues.y = 1;
 	elif(Input.is_action_just_pressed("d")):
 		self.currentDirection = Directions.RIGHT;
-		
+		self.directionValues.x = 1;
 	elif(Input.is_action_just_pressed("s")):
 		self.currentDirection = Directions.DOWN;
+		self.directionValues.y = -1;
 	
+	#playerCood = playerCood.move_toward(self.directionValues, delta*10.0);
+	#playerCood.x += directionValues.x;  # Exemplo com aceleracao
+	#playerCood.y += directionValues.y;  # Exemplo com aceleracao 2
+	print("X :",str(playerCood.x));
 	
 	
 	match currentDirection:
@@ -46,6 +55,9 @@ func _physics_process(delta :float) -> void:
 			self.get_child(count).visible = true;
 		else:
 			self.get_child(count).visible = false;
+			
+			
+	self.move_and_collide(playerCood);
 	
 	
 	
