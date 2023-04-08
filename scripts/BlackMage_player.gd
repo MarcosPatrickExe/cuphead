beb94dc5518c23e39065cc4db6898aa9e9d94fa6
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-04-08 20:41:25 -0300

    Editing a running animations of black mage

--- scripts/BlackMage_player.gd
+++ scripts/BlackMage_player.gd
@@ -14,18 +14,36 @@ func _physics_process(delta :float) -> void:
 	
 	self.directionsValues.x = Input.get_action_strength("d") - Input.get_action_strength("a");
 	self.directionsValues.y = Input.get_action_strength("s") - Input.get_action_strength("w");
-	print("X: ", str(directionsValues.x), "//  Y: ",str(directionsValues.y));
+	
+	# impedindo a personagem de andar na diagonal:
+	if( (directionsValues.x !=0) and (directionsValues.y!=0)):
+		self.directionsValues = Vector2.ZERO;
+	
+	
+	# animacoes para quando o personagem estiver parado:
+	elif( directionsValues == Vector2(0, 0)):
+		match self.currentState:
+			states.DOWN:
+				$AnimationPlayer.play("drinking");
+			
+	
 	
 	self.playerSpeed = playerSpeed.move_toward( directionsValues*2, delta*10 );
-	
+	# movendo o player
 	self.position += self.playerSpeed;
 
 
+	# animacoes de movimentacao:
 	if(directionsValues.x == -1):
 			$AnimationPlayer.play("run_to_left");
-	if(directionsValues.x == 1):
+			self.currentState = states.LEFT;
+	elif(directionsValues.x == 1):
 			$AnimationPlayer.play("run_to_right");
-	if(directionsValues.y == -1):
-		$AnimationPlayer.play("run_to_up");
-	if(directionsValues.y == 1):
+			self.currentState = states.RIGHT;
+	elif(directionsValues.y == -1):
+			$AnimationPlayer.play("run_to_up");
+			self.currentState = states.UP;
+	elif(directionsValues.y == 1):
 			$AnimationPlayer.play("run_to_down");
+			self.currentState = states.DOWN;
+			
