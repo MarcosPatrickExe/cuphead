802fcf1b039c126e865f98437d491e6474ae6c67
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-04-16 00:18:35 -0300

    All attack animations of Fox characters has been addited!

--- player.gd
+++ player.gd
@@ -1,34 +1,25 @@
-extends KinematicBody2D
-
-var playerSpeed :Vector2 = Vector2(0, 0); # Vector2.ZERO;
+extends KinematicBody2D;
+var playerSpeed :Vector2 = Vector2(0, 0); 
 const SPEED :int = 2;
 const FRICTION :int = 20;
 const ACCELERATION :int = 20;
 onready var playerAnimationInstance = $AnimationPlayer; #Instancia o node dentro da funcao "ready()"
 enum states { IDLE_RIGHT, IDLE_DOWN, IDLE_LEFT, IDLE_UP, LEFT_ATTACK, RIGHT_ATTACK, UP_ATTACK, DOWN_ATTACK }
 var currentState;
-const attackDurationLimit = 8 ;
 
 
 #funcao q eh executada quando a animacao de ataque termina, alem de mudar o estado 'ataque' para 'idle':
 func onAttackEndingAnimation():
-	
 	match self.currentState:
 		states.LEFT_ATTACK:
 			self.currentState = self.states.IDLE_LEFT;
 		states.RIGHT_ATTACK:
 			self.currentState = self.states.IDLE_RIGHT;
-	
+		states.UP_ATTACK:
+			self.currentState = self.states.IDLE_UP;
+		states.DOWN_ATTACK:
+			self.currentState = self.states.IDLE_DOWN;
 
-
-func _input(event :InputEvent):
-	if(event.is_action_released("arrow_left") ):
-		self.currentState = self.states.LEFT_ATTACK;
-		playerAnimationInstance.play("left_attack");
-		
-	if(event.is_action_pressed("arrow_right") ):
-		self.currentState = self.states.RIGHT_ATTACK;
-		playerAnimationInstance.play("right_attack");
 
 
 
@@ -47,22 +38,16 @@ func _physics_process(delta :float) -> void:
 			speedResult.x = SPEED;
 			self.currentState = states.IDLE_RIGHT;
 			playerAnimationInstance.play("Run_to_right");
-	#		print(self.get_children()[0].scale);
-
 	elif (Input.is_action_pressed("a")):
 			speedResult.x = -SPEED;
 			self.currentState = states.IDLE_LEFT;
 			playerAnimationInstance.play("Run_to_left");
 	else:	
 			speedResult.x = 0;
-	
-			
 	if (Input.is_action_pressed("w")):
 			speedResult.y = -SPEED;
 			self.currentState = states.IDLE_UP;
 			playerAnimationInstance.play("Run_to_top");
-			
-			
 	elif (Input.is_action_pressed("s")):
 			speedResult.y = SPEED;
 			self.currentState = states.IDLE_DOWN;
@@ -82,13 +67,12 @@ func _physics_process(delta :float) -> void:
 			# em "Vector2.ZERO"
 
 
-	if((playerSpeed == Vector2.ZERO) and (self.currentState != states.RIGHT_ATTACK) and (self.currentState != states.LEFT_ATTACK)  ): #Caso o player esteja parado, ele recebe um frame estatico
-		playerAnimationInstance.stop();
+	if((playerSpeed == Vector2.ZERO) and !self.isAttackingState() ): #Caso o player esteja parado, ele recebe um frame estatico
+		playerAnimationInstance.stop(); #encerrando animacao de correr
 		
-		match self.currentState: #"match" atua como um 'switch' 
+		match self.currentState:
 			states.IDLE_LEFT:
 				 self.get_child(0).frame = 12;
-			#	playerAnimationInstance.play("attack_left");
 			states.IDLE_UP:
 				self.get_child(0).frame = 9;
 			states.IDLE_RIGHT:
@@ -99,3 +83,36 @@ func _physics_process(delta :float) -> void:
 	
 	#	self.position = self.position + playerSpeed;   TBM EQUIVALE À:
 	move_and_collide(playerSpeed);  # aplicando os valores de 'playerSpeed' para a funcao acumulativa
+
+
+
+
+
+func _input(event :InputEvent):
+	if(event.is_action_released("arrow_left") ):
+			self.currentState = self.states.LEFT_ATTACK;
+			playerAnimationInstance.play("left_attack");
+	elif(event.is_action_pressed("arrow_right") ):
+			self.currentState = self.states.RIGHT_ATTACK;
+			playerAnimationInstance.play("right_attack");
+	elif(event.is_action_pressed("arrow_up") ):
+			self.currentState = self.states.UP_ATTACK;
+			playerAnimationInstance.play("up_attack");
+	elif(event.is_action_pressed("arrow_down") ):
+			self.currentState = self.states.DOWN_ATTACK;
+			playerAnimationInstance.play("down_attack");
+
+
+
+
+func isAttackingState() -> bool:
+	match self.currentState:
+		states.LEFT_ATTACK:
+			return true;
+		states.RIGHT_ATTACK:
+			return true;
+		states.UP_ATTACK:
+			return true;
+		states.DOWN_ATTACK:
+			return true;
+	return false;
