b46ff4fb4549161b64757ce88781687373fbeb7a
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-08-08 23:05:49 -0300

    Animations of Ayla and Chrono has been modifyied

--- scripts/Ayla_player.gd
+++ scripts/Ayla_player.gd
@@ -30,13 +30,13 @@ func _physics_process(delta :float) -> void:
 	
 	playerCood = playerCood.move_toward(self.directionValues*6, delta*ACCELERATION); # aqui a velocidade do player vai aumentando (ou diminuindo) ate chegar no valor-alvo, delimitado por "directionValues*6" !!
 	# playerCood = directionValues*6; # nessa versao, a velocidade do player eh constante, ou seja, vale "directionValues*6" !
-	# playerCood.x += directionValues.x;  # aqui a velocidade do player eh acelerada ate o infinito, sem nenhum limite!!
-	
+
+
 	# aqui o "playerCood" eh adicionando a posicao (X, Y) anterior do persoangem
 	self.move_and_collide(playerCood);
 	
 	#print( self.Directions.keys()[self.currentDirection] );
-	#print(self.currentDirection);
+
 
 
 
@@ -52,24 +52,21 @@ func idleAnimations() -> void:
 		Directions.DOWN:
 			self.get_node("Ayla_stopped_down").visible = true;
 			self.animations.play("stopped_down");
-			print("idle down");
 
 		Directions.UP:
 			self.get_node("Ayla_stopped_up").visible = true;
 			self.animations.play("stopped_up");
-			print("idle up");
 			
 		Directions.RIGHT:
 			self.get_node("Ayla_stopped").set_flip_h(false);
 			self.get_node("Ayla_stopped").visible = true;
 			self.animations.play("stopped_left_right");
-			print("idle right");
 
 		Directions.LEFT:
 			self.get_node("Ayla_stopped").set_flip_h(true);
 			self.get_node("Ayla_stopped").visible = true;
 			self.animations.play("stopped_left_right");
-			print("idle left");
+		
 
 
 
@@ -78,7 +75,6 @@ func runAnimations(x :int, y :int) -> void:
 	for count in range(4, 7):
 		get_child(count).visible = false;
 
-	print(x, "//  ", y);
 
 	match x:
 		-1:
@@ -88,7 +84,7 @@ func runAnimations(x :int, y :int) -> void:
 		1:
 			animations.play("run_to_right");
 			self.currentDirection = self.Directions.RIGHT;
-			get_node("Ayla_run_to_right_sprite").visible = true;
+			get_child(2).visible = true;
 	match y:
 		1:
 			animations.play("run_to_down");
